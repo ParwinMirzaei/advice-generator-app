2a74c37c818a9e9a256704a39b6cf858d8e9178f
ParwinMirzaei <parwinmirzaei@yahoo.com>
2025-02-02 10:53:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ async function fetchAdvice() {
     const response = await fetch('https://api.adviceslip.com/advice');
     const data = await response.json();
     document.querySelector('#advice-id').innerText = data.slip.id;
-    document.querySelector('#advice-text').innerText = `${data.slip.advice}`;
+    document.querySelector('#advice-text').innerText = `'${data.slip.advice}'`;
   } catch (error) {
     document.querySelector('#advice-text').innerText = 'Error fetching advice.';
   }
